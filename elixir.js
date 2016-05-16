face32b49fefdd849a64b5d6901df1764a400f58
kristjanjansen <kristjan.jansen@gmail.com>
2016-05-16 12:54:37 +0300

    latest

---
+++ elixir.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var postcss = require('gulp-postcss');
+
+var Elixir = require('laravel-elixir');
+
+var $ = Elixir.Plugins;
+var config = Elixir.config;
+
+Elixir.extend('postcss', function (src, output) {
+
+    return new Elixir.Task('postcss', function () {
+        
+        this.log(src, output);
+
+        return gulp.src(src)
+            .pipe($.concat('main.css'))
+            .pipe(postcss([
+                require('postcss-advanced-variables')(),
+                require('postcss-responsive-type')(),
+                require('postcss-font-magician')(),
+            ]).on('error', function(err) {
+                new Elixir.Notification('PostCSS Failed!');
+                console.log(err)
+                this.emit('end');
+            }))
+            .pipe(gulp.dest('./public/css'))
+            .pipe(new Elixir.Notification('PostCSS Compiled!'));
+    
+    })
+    .watch(src);
+
+});

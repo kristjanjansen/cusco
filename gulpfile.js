c9edf475e85b09e3e9d6e6173da5f6138818d99e
kristjanjansen <kristjan.jansen@gmail.com>
2016-05-16 18:46:30 +0300

    grid

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ elixir(function(mix) {
     mix.browserify('./resources/views/main.js')
 
     mix.postcss([
+        './node_modules/suitcss-components-grid/lib/grid.css',
         './node_modules/vue-animate/dist/vue-animate.css',
         './resources/views/styles/**/*.css',
         './resources/views/components/**/*.*css'
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -8,15 +8,15 @@
     </head>
     <body>
         
-        <div class="row">
+        <div class="Grid Grid--fit">
 
-                <div class="item">
+                <div class="Grid-cell">
 
                     @component('Alert--lg', ['title' => 'I am an alert.'])
 
                 </div>
 
-                <div class="item">
+                <div class="Grid-cell">
 
                     @component('Body', ['body' => '<p>Ennui flannel offal next level pork belly. Fap before they sold out wolf cardigan vegan, waistcoat bitters four loko listicle synth church-key you probably havent heard of them keffiyeh sriracha.</p><h3>Gentrify etsy chartreuse</h3><p>try-hard. Deep v blue bottle four dollar toast, pork belly YOLO direct trade 90 cold-pressed beard photo booth selvage craft beer. Craft beer helvetica portland ethical chia, polaroid salvia 90 cliche tousled. Blue bottle semiotics humblebrag wolf etsy bitters blog, cornhole master cleanse food truck everyday carry pop-up cray lomo. Deep v farm-to-table poutine, cardigan brooklyn godard iPhone post-ironic thundercats authentic bitters.</p>'])
 

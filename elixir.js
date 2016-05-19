56373e99f9bfac06f8b166bda1659a048b74b12b
kristjanjansen <kristjan.jansen@gmail.com>
2016-05-19 14:13:54 +0300

    reset

--- elixir.js
+++ elixir.js
@@ -20,8 +20,9 @@ Elixir.extend('postcss', function (src, output) {
         this.log(src, output);
 
         return gulp.src(src)
-            .pipe($.concat('main.css'))
+            .pipe($.if(config.sourcemaps, $.sourcemaps.init()))
             .pipe(postcss([
+                require('postcss-import')({ path: './resources/views/utils'}),
                 require('postcss-simple-vars')(),
                 require('postcss-responsive-type')(),
                 require('postcss-font-magician')(),
@@ -31,6 +32,8 @@ Elixir.extend('postcss', function (src, output) {
                 console.log(err)
                 this.emit('end');
             }))
+            .pipe($.concat('main.css'))
+            .pipe($.if(config.sourcemaps, $.sourcemaps.write('.')))
             .pipe(gulp.dest(output || './public/css'))
             .pipe(new Elixir.Notification('PostCSS Compiled!'));
     
--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,10 @@ require('./elixir')
 elixir(function(mix) {
 
     mix.browserify('./resources/views/main.js')
-
+    
     mix.postcss([
-        './node_modules/vue-animate/dist/vue-animate.css',
-        './resources/views/variables/**/*.css',
-        './resources/views/utils/**/*.css',
-        './resources/views/components/**/*.*css'
+        './resources/views/components/**/*.css',
+        './resources/views/utils/**/*.css'
     ])
 
 });

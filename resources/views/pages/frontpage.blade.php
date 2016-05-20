765fb4a9b054305ce83c8ff5de3549fb5f2ca597
kristjanjansen <kristjan.jansen@gmail.com>
2016-05-20 10:54:31 +0300

    hmm

--- resources/views/pages/frontpage.blade.php
+++ resources/views/pages/frontpage.blade.php
@@ -32,7 +32,7 @@
 
                 <div class="col padding-left-collapse-sm padding-topbottom-md">
 
-                    @component('Body', ['body' => '<p>Ennui flannel offal next level pork belly. Fap before they sold out wolf cardigan vegan, waistcoat bitters four loko listicle synth church-key you probably havent heard of them keffiyeh sriracha.</p><h3>Gentrify etsy chartreuse</h3><p>try-hard. Deep v blue bottle four dollar toast, pork belly YOLO direct trade 90 cold-pressed beard photo booth selvage craft beer. Craft beer helvetica portland ethical chia, polaroid salvia 90 cliche tousled. Blue bottle semiotics humblebrag wolf etsy bitters blog, cornhole master cleanse food truck everyday carry pop-up cray lomo. Deep v farm-to-table poutine, cardigan brooklyn godard iPhone post-ironic thundercats authentic bitters.</p>'])
+                    @component('Body', ['body' => '<p>Tang Lung arrives in Rome to help his cousins in the restaurant business. They are being pressured to sell their property to the syndicate, who will stop at nothing to get what they want.</p><h3>A new threat</h3><p>When Tang arrives he poses a new threat to the syndicate, and they are unable to defeat him. The syndicate boss hires the best Japanese and European martial artists to fight Tang, but he easily finishes them off. The American martial artist Colt is hired and has a showdown with Tang in Romes famous Colosseum.'])
 
                 </div>
 
@@ -48,7 +48,7 @@
 
         <div class="row">
 
-                <div class="col">
+                <div class="col padding-right-collapse-sm">
 
                     @component('Media--border', [
                         'aside' => component('UserImage', [
@@ -60,14 +60,9 @@
 
                 </div>  
 
-                <div class="col padding-leftright-collapse-md padding-topbottom-md">
+                <div class="col padding-left-collapse-sm padding-topbottom-md">
 
-                    @component('Display', [
-                        'image' => 'http://www.plexmx.info/wp-content/uploads/2015/03/lone-wolf-mcquade-chuck-norris-david-carradine-jpg-1766863898.jpg',
-                        'title' => 'Craft beer helvetica portland...'
-                    ])
-
-                    @component('Body', ['body' => '<p>Ennui flannel offal next level pork belly. Fap before they sold out wolf cardigan vegan, waistcoat bitters four loko listicle synth church-key you probably havent heard of them keffiyeh sriracha.</p><h3>Gentrify etsy chartreuse</h3><p>try-hard. Deep v blue bottle four dollar toast, pork belly YOLO direct trade 90 cold-pressed beard photo booth selvage craft beer. Craft beer helvetica portland ethical chia, polaroid salvia 90 cliche tousled. Blue bottle semiotics humblebrag wolf etsy bitters blog, cornhole master cleanse food truck everyday carry pop-up cray lomo. Deep v farm-to-table poutine, cardigan brooklyn godard iPhone post-ironic thundercats authentic bitters.</p>'])
+                    @component('Body', ['body' => '<p>J.J. McQuade is a Texas Ranger who doesnt exactly follow the rules, is unruly, and prefers to work alone, which earns the nickname, Lone Wolf McQuade. When he discovers some criminals have automatic weapons, he discovers that they were stolen from the military. He tries to handle on his own, as usual, but in the end, an old friend, and a prisoner, whom he was keeping under wraps, are killed. He is then relieved of duty. But then an FBI agent, who also wants to get these guys, offers to help McQuade, and along with a rookie, they track down the mastermind.</p>'])
 
                 </div>
 

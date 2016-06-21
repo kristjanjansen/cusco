e8fd41e0e137c14a5f06cf5032287776e3a6d8b4
kristjanjansen <kristjan.jansen@gmail.com>
2016-06-21 13:41:05 +0300

    checkbox

---
+++ resources/views/components/FormCheckbox/FormCheckbox.blade.php
@@ -0,0 +1,13 @@
+<div class="FormCheckbox {{ $isclasses }}">
+
+    <input
+        id="{{ $name }}"
+        name="{{ $name }}"
+        type="checkbox"
+        @if ($checked) checked @endif
+        class="FormCheckbox__checkbox" 
+    >
+
+    <label for="{{ $name }}" class="FormCheckbox__label">{{ $label }}</label>
+
+</div>
